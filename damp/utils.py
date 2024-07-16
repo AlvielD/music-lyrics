272bcd053093f4b3de1ce10ef5986816d1149641
Álvaro Esteban Muñoz <aesteban.task@gmail.com>
2024-07-16 12:37:17 +0200

    Modularized utils and added a class to manage lyrics annotations

--- damp/utils.py
+++ damp/utils.py
@@ -1,31 +1,43 @@
-import config
-import spotipy
-from spotipy import SpotifyClientCredentials
+import config.genius_config as config
+import requests
 
-class SpotiScraper():
-    def __init__(self):
-        # Inicializar el cliente de Spotify
-        self.sp_client = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-            client_id=config.CLIENT_TOKEN,
-            client_secret=config.CLIENT_SECRET_TOKEN))
+def search_song(song_title, song_artist):
+    """Search for a song on Genius and return the first result
+
+    Args:
+        song_title (str): title of the song to be searched
+
+    Returns:
+        dict: information about the first result of the search
+    """
+    search_url = f'{config.BASE_URL}/search'
+    params = {'q': song_title + ' ' + song_artist}
+    response = requests.get(search_url, headers=config.HEADERS, params=params)
+    data = response.json()
+
+    if data['response']['hits']:
+        return data['response']['hits'][0]['result']
+    else:
+        return None
     
 
-    def search_song_on_spotify(self, song_title, artist_name):
-        query = f"track:{song_title} artist:{artist_name}"
-        result = self.sp_client.search(q=query, type='track', limit=1)
-        return result
+def get_song_metadata(song_id):
+    """Get song's metadata by its Genius ID
+
+    Args:
+        song_id (str): ID of the song
+
+    Returns:
+        dict: metadata of the requested song
+    """
+    song_url = f'{config.BASE_URL}/songs/{song_id}'
+    response = requests.get(song_url, headers=config.HEADERS)
+    return response.json()
 
 
-    def get_song_duration(self, song_title, song_artist):
-        # Buscar la canción en Spotify
-        spotify_response = self.search_song_on_spotify(song_title, song_artist)
-        if not spotify_response['tracks']['items']:
-            print(f"No se encontró la canción {song_title} de {song_artist} en Spotify.")
-            return None
+def get_song_langugage(song_title, song_artist):
+    song = search_song(song_title, song_artist)
+    song_id = song['id']
 
-        # Obtener la duración de la canción
-        track_info = spotify_response['tracks']['items'][0]
-        duration_ms = track_info['duration_ms']
-        duration_sec = duration_ms / 1000
-
-        return duration_sec
+    metadata = get_song_metadata(song_id)['response']
+    return metadata['song']['language']
